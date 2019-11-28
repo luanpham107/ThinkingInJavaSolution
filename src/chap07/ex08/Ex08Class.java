fd2556926c0dff4835dc5d01587a1b6f56bde6ae
Luan(Liam) N. Pham <Liam@enclave.com>
2019-11-28 17:13:00 +0700

    Chap07 Ex05 to ex10

---
+++ src/chap07/ex08/Ex08Class.java
@@ -0,0 +1,24 @@
+package chap07.ex08;
+
+class Ex08ClassBase {
+    Ex08ClassBase(int x){
+        System.out.println("Ex08ClassA base constructor: " + x);
+    }
+}
+
+public class Ex08Class extends Ex08ClassBase{
+
+    Ex08Class(int x){
+        super(0);
+        System.out.println("Derived no arg constructor");
+    }
+
+    Ex08Class(){
+        super(0);
+        System.out.println("Derived constructor");
+    }
+    public static void main(String[] args) {
+        Ex08Class a = new Ex08Class(0);
+        Ex08Class b = new Ex08Class();
+    }
+}
